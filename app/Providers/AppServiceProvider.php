21dce64a994492338e43092ed8d1ee112c8286ca
doctorcmptrmita2 <57236691+doctorcmptrmita2@users.noreply.github.com>
2025-12-23 03:26:50 +0300

    Force HTTPS for all URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Listeners\SendLoginNotification;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,22 @@ class AppServiceProvider extends ServiceProvider
 
         // Register login event listener
         Event::listen(Login::class, SendLoginNotification::class);
+
+        // Force HTTPS for all URLs in production if APP_URL is HTTPS
+        if (config('app.env') === 'production') {
+            $appUrl = config('app.url');
+            $assetUrl = config('app.asset_url') ?: $appUrl;
+            
+            if ($appUrl && str_starts_with($appUrl, 'https://')) {
+                // Force all URLs to use HTTPS
+                URL::forceScheme('https');
+                URL::forceRootUrl($appUrl);
+            }
+            
+            // Override asset URL if ASSET_URL is set
+            if ($assetUrl && str_starts_with($assetUrl, 'https://')) {
+                config(['app.asset_url' => $assetUrl]);
+            }
+        }
     }
 }
